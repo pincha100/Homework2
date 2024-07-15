fcf7a958ad0331087994cbf1654c5eda15d66d9a
pincha100 <113956174+pincha100@users.noreply.github.com>
2024-07-15 17:35:22 +0700

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,5 @@
+Completed_tasks = 12
+Spent_hours = 1.5
+Name = ("Python")
+Time = (Spent_hours / Completed_tasks)
+print("Курс:", Name, "всего задач:", Completed_tasks, "часов затрачено:", Spent_hours, "средние время выполнения", Time, "часа" ".")
